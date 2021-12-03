f9b1027492602a005a873214157c21c75f0589c9
Tony-kim93 <81947946+Tony-kim93@users.noreply.github.com>
2021-12-03 13:42:58 +0900

    완전탐색 level1

---
+++ programmars/search.js
@@ -0,0 +1,84 @@
+// Q1 모의고사
+// 문제
+
+// 문제 설명
+// 수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+
+// 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+// 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+// 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+
+// 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+// 제한 조건
+// 시험은 최대 10,000 문제로 구성되어있습니다.
+// 문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
+// 가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
+
+// solution 1
+
+function solution(answers) {
+  // 학생 a,b,c의 찍는 패턴
+  const a = [1, 2, 3, 4, 5];
+  const b = [2, 1, 2, 3, 2, 4, 2, 5];
+  const c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  // Result 배열에 push
+  let result = [0, 0, 0];
+  for (let i = 0; i < answers.length; i++) {
+    if (a[i % 5] === answers[i]) result[0]++;
+    if (b[i % 8] === answers[i]) result[1]++;
+    if (c[i % 10] === answers[i]) result[2]++;
+  }
+  // 최종 답안 추출
+  const maximum = Math.max(...result);
+  const finalRes = [];
+  let index = 0;
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] === maximum) {
+      finalRes[index] = i + 1;
+      index++;
+    }
+  }
+  return finalRes;
+}
+
+// solution2
+
+function solution(answers) {
+  let answer = [];
+  let a1 = [1, 2, 3, 4, 5];
+  let a2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  let a3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  let a1c = answers.filter((a, i) => a === a1[i % a1.length]).length;
+  let a2c = answers.filter((a, i) => a === a2[i % a2.length]).length;
+  let a3c = answers.filter((a, i) => a === a3[i % a3.length]).length;
+  let max = Math.max(a1c, a2c, a3c);
+
+  if (a1c === max) {
+    answer.push(1);
+  }
+  if (a2c === max) {
+    answer.push(2);
+  }
+  if (a3c === max) {
+    answer.push(3);
+  }
+  return answer;
+}
+
+// Q2 소수 찾기
+
+// 문제 설명
+// 한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 흩어진 종이 조각을 붙여 소수를 몇 개 만들 수 있는지 알아내려 합니다.
+
+// 각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// numbers는 길이 1 이상 7 이하인 문자열입니다.
+// numbers는 0~9까지 숫자만으로 이루어져 있습니다.
+// "013"은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.
+
+// solution1
+
+// solution2
