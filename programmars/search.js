15ba267e95d9a9c05290f881a49aa592b03c6048
Tony-kim93 <81947946+Tony-kim93@users.noreply.github.com>
2021-12-06 11:45:55 +0900

    소수찾기

--- programmars/search.js
+++ programmars/search.js
@@ -81,4 +81,72 @@ function solution(answers) {
 
 // solution1
 
+function solution(numbers) {
+  const arr = numbers.split("");
+  const answer = new Set();
+  getNumber(arr, "");
+  function getNumber(numbersArr, currentNumber) {
+    if (numbersArr.length) {
+      for (let i = 0; i < numbersArr.length; i++) {
+        const temp = [...numbersArr];
+        temp.splice(i, 1);
+        if (isPrime(parseInt(currentNumber + numbersArr[i]))) {
+          answer.add(parseInt(currentNumber + numbersArr[i]));
+        }
+        getNumber(temp, currentNumber + numbersArr[i]);
+      }
+    }
+  }
+
+  function isPrime(num) {
+    if (num < 2) return false;
+    if (num === 2) return true;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
+
+  return answer.size;
+}
+
 // solution2
+
+function solution(numbers) {
+  var answer = 0;
+
+  var n = numbers.split("");
+  var nums = new Set();
+  combi(n, "");
+
+  function combi(a, s) {
+    if (s.length > 0) {
+      if (nums.has(Number(s)) === false) {
+        nums.add(Number(s));
+        console.log(Number(s));
+        if (chkPrime(Number(s))) {
+          answer++;
+        }
+      }
+    }
+    if (a.length > 0) {
+      for (var i = 0; i < a.length; i++) {
+        var t = a.slice(0);
+        t.splice(i, 1);
+        //console.log(t)
+        combi(t, s + a[i]);
+      }
+    }
+  }
+
+  function chkPrime(num) {
+    if (num < 2) return false;
+    if (num === 2) return true;
+    for (var i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
+
+  return answer;
+}
