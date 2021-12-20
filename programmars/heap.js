210464f40eecb1676cfe740381250a5c025c5ad8
Tony-kim93 <81947946+Tony-kim93@users.noreply.github.com>
2021-12-20 12:00:56 +0900

    heap q1

---
+++ programmars/heap.js
@@ -0,0 +1,14 @@
+// Q1 더 맵게(Python)
+
+// import heapq
+
+// def solution(scoville, k):
+//     heapq.heapify(scoville)
+//     i = 0
+//     while scoville[0] < k:
+//         if len(scoville) > 1:
+//             heapq.heappush(scoville, heapq.heappop(scoville) + (heapq.heappop(scoville) * 2))
+//             i += 1
+//         else:
+//             return -1
+//     return i
